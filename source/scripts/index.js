a2593b362f026ead442802daf4c94d0380d0ba5d
Игорь <90267795+igoryha96@users.noreply.github.com>
2024-05-19 22:25:44 +0300

    Модальное окно с js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+let modalToggle = document.querySelector(".main-header__button");
+let modalMenu = document.querySelector(".modal-menu");
+console.log(modalToggle);
+
+modalToggle.classList.remove("main-header__button--nojs");
+modalMenu.classList.remove("modal-menu--nojs-menu");
+
+modalToggle.onclick = function () {
+  modalToggle.classList.toggle("main-header__button--closed");
+  modalToggle.classList.toggle("main-header__button--open");
+  modalMenu.classList.toggle("modal-menu--closed");
+  modalMenu.classList.toggle("modal-menu--open");
+};
