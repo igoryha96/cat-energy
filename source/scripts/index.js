79e59d198a9744fc3918f72777553b50517a5e27
Игорь <90267795+igoryha96@users.noreply.github.com>
2024-05-30 00:33:59 +0300

    Добавляет интерактивную карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,12 @@
 /* в этот файл добавляет скрипты*/
 let modalToggle = document.querySelector(".main-header__button");
 let modalMenu = document.querySelector(".modal-menu");
+let maps = document.querySelector(".map");
 console.log(modalToggle);
 
 modalToggle.classList.remove("main-header__button--nojs");
 modalMenu.classList.remove("modal-menu--nojs-menu");
+maps.classList.remove("map--nojs");
 
 modalToggle.onclick = function () {
   modalToggle.classList.toggle("main-header__button--closed");
