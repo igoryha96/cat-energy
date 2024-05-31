23c59a670973b2173a3c75dc15f73289c9961e04
Игорь <90267795+igoryha96@users.noreply.github.com>
2024-05-31 20:04:48 +0300

    Исправления для линтера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,15 @@
 /* в этот файл добавляет скрипты*/
-let modalToggle = document.querySelector(".main-header__button");
-let modalMenu = document.querySelector(".modal-menu");
-let maps = document.querySelector(".map");
-console.log(modalToggle);
+const modalToggle = document.querySelector('.main-header__button');
+const modalMenu = document.querySelector('.modal-menu');
+const maps = document.querySelector('.map');
 
-modalToggle.classList.remove("main-header__button--nojs");
-modalMenu.classList.remove("modal-menu--nojs-menu");
-maps.classList.remove("map--nojs");
+modalToggle.classList.remove('main-header__button--nojs');
+modalMenu.classList.remove('modal-menu--nojs-menu');
+maps.classList.remove('map--nojs');
 
 modalToggle.onclick = function () {
-  modalToggle.classList.toggle("main-header__button--closed");
-  modalToggle.classList.toggle("main-header__button--open");
-  modalMenu.classList.toggle("modal-menu--closed");
-  modalMenu.classList.toggle("modal-menu--open");
+  modalToggle.classList.toggle('main-header__button--closed');
+  modalToggle.classList.toggle('main-header__button--open');
+  modalMenu.classList.toggle('modal-menu--closed');
+  modalMenu.classList.toggle('modal-menu--open');
 };
