123a19118c103aed7f30160cbb89359d2c1883e2
Игорь <90267795+igoryha96@users.noreply.github.com>
2024-05-31 20:06:15 +0300

    Module7 task1 (#11)
    * Модальное окно с js
    
    * Пиксель шапки
    
    * Пиксель перфект главной страницы почти
    
    * пиксель главной
    
    * Карточки каталога мобилка
    
    * Пиксель каталога
    
    * Пиксель предпоследний блок
    
    * пиксель перфект
    
    * Добавляет интерактивную карту
    
    * Добавляет локацию в каталог
    
    * Вроде доделала
    
    * Правки после зозвона
    
    * Удаляет пустые классы
    
    * исправляет ошибку переменной
    
    * исправляет прошлый комит
    
    * Исправления для линтера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const modalToggle = document.querySelector('.main-header__button');
+const modalMenu = document.querySelector('.modal-menu');
+const maps = document.querySelector('.map');
+
+modalToggle.classList.remove('main-header__button--nojs');
+modalMenu.classList.remove('modal-menu--nojs-menu');
+maps.classList.remove('map--nojs');
+
+modalToggle.onclick = function () {
+  modalToggle.classList.toggle('main-header__button--closed');
+  modalToggle.classList.toggle('main-header__button--open');
+  modalMenu.classList.toggle('modal-menu--closed');
+  modalMenu.classList.toggle('modal-menu--open');
+};
