74dd4bd719b58836c63aa3dc57b22e83fba25081
Игорь <90267795+igoryha96@users.noreply.github.com>
2024-06-04 00:46:41 +0300

    исправлениея после проверки на допуска

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,10 +2,12 @@
 const modalToggle = document.querySelector('.main-header__button');
 const modalMenu = document.querySelector('.modal-menu');
 const maps = document.querySelector('.map');
+const mapsGoogle = document.querySelector('.map-google');
 
 modalToggle.classList.remove('main-header__button--nojs');
 modalMenu.classList.remove('modal-menu--nojs-menu');
 maps.classList.remove('map--nojs');
+mapsGoogle.classList.remove('map-google--no-js-map');
 
 modalToggle.onclick = function () {
   modalToggle.classList.toggle('main-header__button--closed');
